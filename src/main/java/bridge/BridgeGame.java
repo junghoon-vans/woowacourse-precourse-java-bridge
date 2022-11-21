88c557d0d647cab945c94bd5cc2b26ec44a68b7d
Jeonghun-Ban <junghoon.ban@gmail.com>
2022-11-21 15:47:59 +0900

    feat: Add start method to bridge game

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -14,6 +14,13 @@ public class BridgeGame {
     }
 
     /**
+     * 사용지가 게임을 시작할 떄 사용하는 메서드
+     */
+    public void start() {
+        this.outputView.printStartGame();
+    }
+
+    /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
