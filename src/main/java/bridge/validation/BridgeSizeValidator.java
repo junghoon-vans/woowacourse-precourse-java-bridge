bfd3cbc1f5647ec68935ec26f17f650d5543e88f
Jeonghun-Ban <junghoon.ban@gmail.com>
2022-11-22 13:12:50 +0900

    feat: Add validate range method

--- src/main/java/bridge/validation/BridgeSizeValidator.java
+++ src/main/java/bridge/validation/BridgeSizeValidator.java
@@ -1,10 +1,15 @@
 package bridge.validation;
 
+import static bridge.Constants.BRIDGE_MAXIMUM_SIZE;
+import static bridge.Constants.BRIDGE_MINIMUM_SIZE;
+
 public class BridgeSizeValidator implements Validator {
 
   @Override
   public void validate(String input) {
     validateNumeric(input);
+    int bridgeSize = Integer.parseInt(input);
+    validateRange(bridgeSize);
   }
 
   /**
@@ -16,6 +21,16 @@ public class BridgeSizeValidator implements Validator {
       Double.parseDouble(input);
     } catch(NumberFormatException e){
       throw new IllegalArgumentException("[ERROR] 다리 길이는 숫자 자료형으로 입력하여야 합니다.");
+    }
+  }
+
+  /**
+   * Verify that the number is between the allowed values.
+   * @param bridgeSize the number to be checked.
+   */
+  private void validateRange(Integer bridgeSize) {
+    if (bridgeSize < BRIDGE_MINIMUM_SIZE || bridgeSize > BRIDGE_MAXIMUM_SIZE) {
+      throw new IllegalArgumentException("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
     }
   }
 }
