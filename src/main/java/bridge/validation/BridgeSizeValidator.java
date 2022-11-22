740892ddeb3c186323c8e515d98d0bd274ca01bf
Jeonghun-Ban <junghoon.ban@gmail.com>
2022-11-22 11:47:08 +0900

    feat: Add validate numeric method

--- src/main/java/bridge/validation/BridgeSizeValidator.java
+++ src/main/java/bridge/validation/BridgeSizeValidator.java
@@ -4,5 +4,18 @@ public class BridgeSizeValidator implements Validator {
 
   @Override
   public void validate(String input) {
+    validateNumeric(input);
+  }
+
+  /**
+   * Verify string is numeric.
+   * @param input the string to be checked.
+   */
+  protected void validateNumeric(String input) {
+    try {
+      Double.parseDouble(input);
+    } catch(NumberFormatException e){
+      throw new IllegalArgumentException("[ERROR] 다리 길이는 숫자 자료형으로 입력하여야 합니다.");
+    }
   }
 }
