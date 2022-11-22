a219b5ac941fb588ad60cc33c362f8ba14e0915e
Jeonghun-Ban <junghoon.ban@gmail.com>
2022-11-22 15:02:59 +0900

    refactor: Add try-catch statement to main method

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -3,7 +3,11 @@ package bridge;
 public class Application {
 
     public static void main(String[] args) {
-        BridgeGame bridgeGame = new BridgeGame();
-        bridgeGame.start();
+        try {
+            BridgeGame bridgeGame = new BridgeGame();
+            bridgeGame.start();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
     }
 }
